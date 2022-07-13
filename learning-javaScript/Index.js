41b0d83c5448b02a94b50402fed1dc0562a3785c
Murali <muralivvrsn75683@gmail.com>
2022-07-13 14:49:42 +0530

    s

---
+++ learning-javaScript/Index.js
@@ -0,0 +1,8 @@
+let a_input = document.getElementById("a-input")
+let b_input = document.getElementById("b-input")
+let c_input = document.getElementById("c-input")
+function solve(){
+    let determinent = (b_input*b_input)-4*(a_input)*(b_input)
+    
+    
+}
