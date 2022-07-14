2523cb48de13894b42abbdbe43f4d6253957d432
Murali <muralivvrsn75683@gmail.com>
2022-07-14 23:56:05 +0530

    sf

--- learning-javaScript/Index.js
+++ learning-javaScript/Index.js
@@ -1,8 +1,15 @@
-let a_input = document.getElementById("a-input")
-let b_input = document.getElementById("b-input")
-let c_input = document.getElementById("c-input")
+let a_input = 2
+let b_input = 4
+let c_input = 8
 function solve(){
     let determinent = (b_input*b_input)-4*(a_input)*(b_input)
-    
+    if(determinent>=0){
+        document.getElementById("x-sol").innerText = (-b_input+(determinent))/(2*a_input)
+        document.getElementById("y-sol").innerText = (-b_input-(determinent))/(2*a_input)
+    }
+    else{
+        document.getElementById("x-sol").innerText = (-b_input)+"+ i"+abs(determinent)+"/"+(2*a_input);
+        document.getElementById("y-sol").innerText = (-b_input)+"- i"+abs(determinent)+"/"+(2*a_input);
+    }
     
 }
