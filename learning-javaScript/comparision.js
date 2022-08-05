5f85bc5a369658a582d8a56ce2fa4a8e4a5b92a9
Murali <muralivvrsn75683@gmail.com>
2022-08-05 18:31:39 +0530

    js

--- learning-javaScript/comparision.js
+++ learning-javaScript/comparision.js
@@ -1,8 +1,8 @@
 console.log("Comparision operator");
 console.log(" 5 > 1 = ", 5 > 1); // greater than 
 console.log(" 5 < 1 = ", 5 < 1);  // less than
-console.log(" 5 == 5 =",5 == 5);  // equal to
+console.log(" 5 == 5 =", 5 == 5);  // equal to
 console.log(" 5 === '5' = ", 5 === '5'); // strict equal to
-console.log("5 != 4 =", 5!=4); // not equal to
-console.log("5 >= 4 =",5 >= 4); // greater than or equal to
-console.log("5 <= 6 =",5 <= 6); // less than or equal to
+console.log("5 != 4 =", 5 != 4); // not equal to
+console.log("5 >= 4 =", 5 >= 4); // greater than or equal to
+console.log("5 <= 6 =", 5 <= 6); // less than or equal to
