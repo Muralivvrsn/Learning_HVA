9d12246faa8069d88310122c7124d8c5ff32fd37
Murali <muralivvrsn75683@gmail.com>
2022-08-18 11:22:21 +0530

    added variables

--- Calculator/Index.js
+++ Calculator/Index.js
@@ -1,4 +1,3 @@
 const firstNumber = parseInt(prompt('Enter First Number'));
 const secondNumber = parseInt(prompt('Enter Second Number'));
 const operation = prompt('Enter Operation (+,-,%,/,*)');
-alert((operation === '+' ? (a + b) : (operation == '-' ? a - b : (operation == '%' ? a % b : (operation == '/' ? a / b : (operation == '*' ? a * b : 'Invalid operator'))))));
