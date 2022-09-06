370b8854df9d946a238acdcfe66150da09568e13
Murali <muralivvrsn75683@gmail.com>
2022-09-06 17:56:20 +0530

    removing settimeout

--- Project-3(Calculator)/app.js
+++ Project-3(Calculator)/app.js
@@ -4,16 +4,8 @@ const answer = document.getElementById('btn15');
 const backSpace = document.getElementById('btn18');
 const input = document.getElementById('enter');
 let str = "";
-function changeColor(element) {
-    element.style.backgroundColor = 'white';
-}
-function changeColor1(element){
-    element.style.backgroundColor = 'red';
-    setTimeout(changeColor(element),1000);
-}
 buttons.forEach(element => {
     element.addEventListener('click', () => {
-        setTimeout(changeColor1(element),1000);
         if(element!==clear && element!==answer && element!==backSpace){
             str+=element.textContent;
         }
