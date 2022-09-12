88d882ca6d966bd68413b1e17719c8a5c2a07ecc
Murali <muralivvrsn75683@gmail.com>
2022-09-12 18:53:33 +0530

    first commit

--- Project-3(Calculator)/app.js
+++ Project-3(Calculator)/app.js
@@ -4,16 +4,17 @@ const answer = document.getElementById('btn15');
 const backSpace = document.getElementById('btn18');
 const input = document.getElementById('enter');
 let str = "";
+let InputContent = "";
 buttons.forEach(element => {
     element.addEventListener('click', () => {
         if(element!==clear && element!==answer && element!==backSpace){
-            str+=element.textContent;
+            str+=element.value;
         }
         else if(element===clear){
             str="";
         }
-        else if(element===answer){
-            str=eval(str);
+        else if(element===answer && str!==""){
+            str=eval(str).toString();
         }
         else{
             str=str.slice(0,-1);
