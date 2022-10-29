d3ab0940da0411d1eeda66b0b7d1d04f24132d13
Murali <muralivvrsn75683@gmail.com>
2022-10-29 10:22:58 +0530

    created react app

--- Project-4(Weather-app)/app.js
+++ Project-4(Weather-app)/app.js
@@ -1,5 +1,5 @@
 // declaration section
-let name;
+let name,cel,far;
 const load=document.getElementById('class-load');
 const videoArray = ['Nature.mp4','Rainy.mp4','Sheet.mp4','Snowflakes.mp4','Summer.mp4','Sunrise.mp4'];
 const apiKey='8c88d8c6dff2e7dd93338bd93db8a4e2';
@@ -7,11 +7,12 @@ const key='27941359-4734624c8d28c40a87b08699f';
 const body=document.getElementById('root');
 const input = document.getElementById('input');
 const main = document.getElementById('main');
-const celcius = document.getElementById('celcius');
-const foreign = document.getElementById('foreign');
+const celcius = document.getElementById('cel');
+const foreign = document.getElementById('far');
 const details = document.getElementById('details');
 const button = document.getElementById('btn-search');
 const climate = document.getElementById('climate');
+const change = document.getElementById('change');
 function getDetails(name){
     let url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${name}`;
     load.style.display='block';
@@ -34,13 +35,13 @@ function getDetails(name){
                     body.style.background=`url(${imageurl['largeImageURL']})`+'no-repeat center center fixed';
                     body.style.backgroundSize='cover';
                 }
-                console.log(Object.keys(imgurl).length)
             })
-            let detailsOfcity = `<p>${data['location'].name}</p><p>${data['location'].lat}</p><p>${data['location'].lon}</p><p>${data['location'].localtime}</p>`
+            cel= data['current'].temperature
+            far= Math.floor(((data['current'].temperature)*1.8)+32)
+            let detailsOfcity = `<p>${data['request'].query}</p><p>${data['location'].lat}</p><p>${data['location'].lon}</p><p>${data['location'].localtime}</p>`
             details.innerHTML = detailsOfcity;
             climate.innerHTML = data['current'].weather_descriptions[0];
-            celcius.innerHTML = `${data['current'].temperature}<sup>o</sup>C`;
-            foreign.innerHTML = `${Math.floor(((data['current'].temperature)*1.8)+32)}<sup>o</sup>F`;
+            change.innerHTML = cel;
             main.style.display='block';
         }
         load.style.display='none';
@@ -57,4 +58,15 @@ function inputTaken(){
     console.log(name);
     getDetails(name);
 }
-
+function changeToCelcius(){
+    change.innerHTML = `${cel}`;
+    celcius.style.fontWeight = 'bolder';
+    foreign.style.fontWeight = '100';
+}
+function changeToFarenhiet(){
+    change.innerHTML = `${far}`;
+    celcius.style.fontWeight = '100';
+    foreign.style.fontWeight = 'bolder';
+}
+celcius.addEventListener('click', changeToCelcius);
+foreign.addEventListener('click', changeToFarenhiet);
