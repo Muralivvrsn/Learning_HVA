6bc35fa1d92552f7fc1eb716dcf474e18dcc44f8
Murali <muralivvrsn75683@gmail.com>
2023-01-20 11:08:56 +0530

    eventEmitter

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,6 +1,19 @@
-const fs = require("fs");
-const file = fs.readFileSync("app.txt", "utf8");
-console.log(file);
-fs.unlink('app.txt',function(err,data){
-    console.log(data);
+const { EventEmitter } = require("events");
+const emitter = new EventEmitter();
+answers = [];
+questions = ["what is your name?", "what is your age?", "what's up?"];
+emitter.on("answer", function (question) {
+  if (answers.length < questions.length) {
+    console.log(question);
+    process.stdin.on("data", (data) => {
+      let answer = data.toString().trim();
+      answers.push(answer);
+      console.log(`The answer is ${answer}`);
+      emitter.emit("answer", questions[answers.length]);
+    });
+  } else {
+    console.log(answers);
+    process.exit();
+  }
 });
+emitter.emit("answer", questions[0]);
