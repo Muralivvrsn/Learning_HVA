201b9ac8199c36f5be74086021da4d5f8e2d7045
Murali <muralivvrsn75683@gmail.com>
2023-02-19 23:27:03 +0530

    middleware, importing modules

--- learning-Nodejs/app.js
+++ learning-Nodejs/app.js
@@ -1,6 +1,7 @@
+
 const express = require('express');
 const app = express();
-
+app.use('/api',require("./index"));
 app.listen(process.env.port || 3000,function(){
     console.log("listening to port 3000....")
-})
+});
