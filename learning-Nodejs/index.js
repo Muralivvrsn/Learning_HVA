d1ba5ed8a19498161deeab2436a904042786f7cc
Murali <muralivvrsn75683@gmail.com>
2023-02-20 17:44:22 +0530

    created mongoose

--- learning-Nodejs/index.js
+++ learning-Nodejs/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const router = express.Router();
-const newPosts = require("./mongoose");
-router.get("/post",function(req,res){
-    res.send({type: 'GET'});
-})
-router.post('/post',function(req,res){
-    newPosts.create(req.body).then(function(newPost){
-        res.send(newPost);
-    });
-});
-router.put('/post/:id',function(req,res){
-    res.send({type: "PUT"});
-});
-router.delete("/post/:id",function(req,res){
-    res.send({type: "DELETE"});
-})
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-module.exports = router;
+// set up express app
+const app = express();
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
+// use body-parser middleware
+app.use(bodyParser.json());
+
+// initialize routes
+app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
+
+// listen for requests
+app.listen(process.env.port || 4000, function(){
+    console.log('now listening for requests');
+});
